198f78dd6e98b496ab9192b1294d429a013b3ee2
Poliana Correia <poliana.karenn05@gmail.com>
2021-11-27 13:17:47 -0300

    dom

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,30 @@
-// getElementById
-//const element = document.getElementById('meu-blog')
+// EXEMPLOS DE .GETELEMENT(S):
+
+// getElementById   (element)
+//const element = document.getElementById('meu-blog');
 //console.log(element)
 
-//getElementByClassNamy
+//getElementByClassName   (HTMLcollection)
+//const element = document.getElementsByClassName('one');
+//console.log(element)
+
+//getElementByTagName     (HTMLcollection)
+//const element = document.getElementsByTagName('head');
+//console.log(element)
+
+
+
+
+// EXEMPLO DE .QUERYSELECTOR:
+//querySelector     (element)
+//const element = document.querySelector('[src]');
+//console.log(element)
+
+//querySelectorAll  (Nodelist)
+const elements = document.querySelectorAll('.one');
+
+elements.forEach(el => console.log(el))
+
+// for (let index = 0; index < elements.length; index++) {
+//     console.log(elements[index])    
+// }
