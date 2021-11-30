c705260d0a10d5b13fb892264c7b59c8ce09e549
Poliana Correia <poliana.karenn05@gmail.com>
2021-11-30 10:36:22 -0300

    exercicio

---
+++ DOM/exercicio.js
@@ -0,0 +1,23 @@
+// Dando funcinalidade ao botão
+
+const buttonOpneModal = document.getElementById
+('openModal')
+
+const modalWrapper = document.querySelector
+('.modal-wrapper')
+
+buttonOpenModal.onclick = function() {
+    modalWrapper
+        .classList
+        .remove('invisible')
+}
+
+document.addEventListener('keydown', function(event) {
+
+    const isEscKey = event.key === 'Escape'
+
+    if(isEscKey) {
+        modalWrapper.classList.add('invisible')
+    }
+})
+
